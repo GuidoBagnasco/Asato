be281a286a68fab240ea37cbc96e0df552f5c2c4
Ale Aran <alearan93@gmail.com>
2018-04-23 21:43:55 -0300

    Basic Enemies
    Basic stuff

---
+++ Asato/Assets/Scripts/Enemies/ShootingEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : EnemyBase {
+    float moveTimer = 2;
+    protected ParticleSystem _balaE;
+    // Use this for initialization
+
+    protected override void Start() {
+        base.Start();
+        _balaE = GetComponentInChildren<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (moveTimer >= 0)
+        {
+            moveTimer -= Time.deltaTime * Time.timeScale;
+            moveTowardsPlayer();
+        }
+        else
+          Invoke("fire", 1);
+    }
+
+    void moveTowardsPlayer()
+    {
+        transform.LookAt(player.transform);
+        transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+    }
+
+    void fire()
+    {
+        _balaE.Emit(1);
+        moveTimer = 2;
+    }
+}

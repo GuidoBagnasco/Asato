26a27f8ffe3dcf1bf73a563617afa929037d047a
バニャスコ・ギード <gbagnasco@tekgenesis.com>
2018-05-17 22:03:51 -0300

    Singleton

---
+++ Asato/Assets/Scripts/Input/IInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInput {
+
+	bool Attack ();
+
+	Vector2 Walk ();
+
+	Vector2 Rotate ();
+
+	bool Jump ();
+
+	int ChangeWeapon ();
+
+}

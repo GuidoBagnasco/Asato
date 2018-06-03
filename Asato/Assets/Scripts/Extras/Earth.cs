6a15d8848a5ca3ab2c7c00ac229d809f76ad52e0
バニャースコ　ギード <guido.bag2.1@gmail.com>
2018-06-03 15:22:37 -0300

    Skybox, terrain, lights, Earth

---
+++ Asato/Assets/Scripts/Extras/Earth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour {
+
+	private float rotSpeed = 4.0f;
+
+
+	private void FixedUpdate () {
+		transform.Rotate (-transform.right - transform.up * 0.01f, rotSpeed * Time.deltaTime);
+	}
+}

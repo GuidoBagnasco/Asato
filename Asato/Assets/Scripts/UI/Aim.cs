0d6904b8744fab92f8792d2056401dc4d9ab43a3
バニャスコ・ギード <gbagnasco@tekgenesis.com>
2018-06-24 15:22:19 -0300

    ...

---
+++ Asato/Assets/Scripts/UI/Aim.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+
+	private UnityEngine.UI.Image aimImage;
+	private UnityEngine.UI.Image flowerImage;
+	public Sprite[] designs;
+	private AimManager aimManager;
+
+	private bool filling = false;
+
+
+
+	private void Awake () {
+		aimImage = GetComponent<UnityEngine.UI.Image> ();
+		flowerImage = GetComponentInChildren<UnityEngine.UI.Image> ();
+	}
+
+
+	private void Start () {
+		aimManager = AimManager.Instance as AimManager;
+	}
+
+
+	private void Update () {
+		SetDesign ();
+		#if UNITY_STANDALONE
+		CheckLock ();
+		#endif
+	}
+
+
+	private void SetDesign () {
+		aimImage.sprite = designs [(int)aimManager.GetCurrentType ()];
+	}
+
+
+	private void CheckLock () {
+		if (!filling && aimManager.IsLocked ()) {
+			StartCoroutine (FillFlower ());
+		} else {
+			StopCoroutine (FillFlower ());
+			filling = false;
+		}
+	}
+
+
+	private IEnumerator FillFlower () {
+		yield return new WaitForFixedUpdate ();
+		filling = true;
+		flowerImage.fillAmount = 0.0f;
+		do {
+			flowerImage.fillAmount = Mathf.Clamp01 (flowerImage.fillAmount + aimManager.Speed () * Time.fixedDeltaTime);
+		} while (flowerImage.fillAmount < 1.0f);
+		filling = false;
+	}
+}
---
+++ Asato/Assets/Scripts/UI/AimManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimManager : Singleton<AimManager> {
+
+	private WeaponSelector wSelector;
+
+
+
+	private void Start () {
+		wSelector = WeaponSelector.Instance as WeaponSelector;
+	}
+	
+
+	public Weapon.WeaponType GetCurrentType () { return wSelector.GetCurrent ().type; }
+
+
+	public bool IsLocked () { return wSelector.GetCurrent ().IsLocked (); }
+
+
+	public float Speed () { return wSelector.GetCurrent ().getFireRate () / 100.0f; }
+
+}
